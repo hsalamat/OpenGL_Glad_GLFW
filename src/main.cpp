936309099ebc82987a453866fd5d91d31aaadd21
hsalamat <2hooman.salamat@gmail.com>
2025-09-03 15:24:58 -0400

    Week2

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,70 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <cstdlib>
-#include <random>
 
-constexpr int SCREEN_WIDTH = 1920;
-constexpr int SCREEN_HEIGHT = 1080;
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+constexpr int SCREEN_WIDTH = 1280;
+constexpr int SCREEN_HEIGHT = 720;
 
 float Random(float min, float max)
 {
-    float random = ((float)rand()) / (float)RAND_MAX;
-    float diff = max - min;
-    float r = random * diff;
-    return min + r;
+    return min + (rand() / ((float)RAND_MAX / (max - min)));
 }
 
+void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+GLuint CreateShader(GLint type, const char* path);
+GLuint CreateProgram(GLuint vs, GLuint fs);
 
 int main(void)
 {
-    GLFWwindow* window;
+    assert(glfwInit() == GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+#ifdef NDEBUG
+#else
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#endif
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graphics 1", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graphics 1", NULL, NULL);
     glfwMakeContextCurrent(window);
+    assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
+    glfwSetKeyCallback(window, key_callback);
 
-    /* Load opengl extension 4.6 (Windows ships with GL 1.0)*/
-    gladLoadGL();
+#ifdef NDEBUG
+#else
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(glDebugOutput, nullptr);
+#endif
 
-	glfwSetKeyCallback(window, key_callback);
+    GLuint vs = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/default.vert");
+    GLuint fs = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/color.frag");
+    GLuint shader = CreateProgram(vs, fs);
 
-    /* Loop until the user closes the window (our game loop, like update function in Unity) */
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.22f, 0.49f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		//Randomly move the mouse cursor
-		float x = Random(0.0f, SCREEN_WIDTH);
-		float y = Random(0.0f, SCREEN_HEIGHT);
-		glfwSetCursorPos(window, x, y);
+        float x = Random(0.0f, SCREEN_WIDTH);
+        float y = Random(0.0f, SCREEN_HEIGHT);
+        //glfwSetCursorPos(window, x, y);
 
-        /* Swap front and back buffers (this avoid screen tear)*/
+        // Uncomment to test debug output -- invalid to render without first uploading vertex data!
+        //glDrawArrays(GL_ARRAY_BUFFER, 0, 3);
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events (check if any buttons has been clicked)*/
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
@@ -66,16 +74,129 @@ int main(void)
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_E && action == GLFW_PRESS)
-    { //activate_airship();
+    const char* name = glfwGetKeyName(key, scancode);
+    if (action == GLFW_PRESS)
+        printf("%s\n", name);
+
+    if (key == GLFW_KEY_ESCAPE)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    // Homework: find a way to consume key inputs to make this an even better virus!
+}
+
+GLuint CreateShader(GLint type, const char* path)
+{
+    GLuint shader = GL_NONE;
+    try
+    {
+        // Load text file
+        std::ifstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(path);
+
+        // Interpret the file as a giant string
+        std::stringstream stream;
+        stream << file.rdbuf();
+        file.close();
+
+        // Verify shader type matches shader file extension
+        const char* ext = strrchr(path, '.');
+        switch (type)
+        {
+        case GL_VERTEX_SHADER:
+            assert(strcmp(ext, ".vert") == 0);
+            break;
+
+        case GL_FRAGMENT_SHADER:
+            assert(strcmp(ext, ".frag") == 0);
+            break;
+        default:
+            assert(false, "Invalid shader type");
+            break;
+        }
+
+        // Compile text as a shader
+        std::string str = stream.str();
+        const char* src = str.c_str();
+        shader = glCreateShader(type);
+        glShaderSource(shader, 1, &src, NULL);
+        glCompileShader(shader);
+
+        // Check for compilation errors
+        GLint success;
+        GLchar infoLog[512];
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cout << "Shader failed to compile: \n" << infoLog << std::endl;
+        }
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "Shader (" << path << ") not found: " << e.what() << std::endl;
     }
 
-    if(key == GLFW_KEY_ESCAPE)
+    return shader;
+}
+
+GLuint CreateProgram(GLuint vs, GLuint fs)
+{
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+
+    // Check for linking errors
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        program = GL_NONE;
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    return program;
+}
+
+void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam)
+{
+    // ignore non-significant error/warning codes
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source)
     {
-        glfwSetWindowShouldClose(window, true);
-	}
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
 
-	const char* name = glfwGetKeyName(key, scancode);
-	printf("Key pressed: %s\n", name);
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
 
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
 }
